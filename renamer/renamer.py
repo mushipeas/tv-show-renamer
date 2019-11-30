922afdff83a491adc2f9c06633ebf00795378481
mushipeas <muj990@gmail.com>
2019-11-30 22:32:19 +0000

    cleaner code

--- renamer/renamer.py
+++ renamer/renamer.py
@@ -23,23 +23,21 @@ class Renamer:
         self.MAKEWINSAFE = MAKEWINSAFE
         self.OUTPUT_FORMAT_STRING = OUTPUT_FORMAT_STRING
         self.season_dir_format = season_dir_format
+        
         self.prog = re.compile(pattern, re.IGNORECASE)
         self.t = tvdb_api.Tvdb()
 
     def _get_regex_tv_info(self, video_filename: str):
         match = self.prog.match(video_filename)
 
-        output = Cobjs.Episode(
-            original_file_name = match.string,
-            id = None,
-            text_show_title = match.group(1).replace('.',' '),
-            tvdb_show_title = None,
-            show_year = match.group(2),
-            season_no = match.group(3),
-            episode_no = match.group(4),
-            episode_title = None,
-            extension = match.string.split('.')[-1]
-        ) if match else None
+        output = {
+            'original_file_name' : match.string,
+            'regex_show_title' : match.group(1).replace('.',' '),
+            'regex_show_year' : match.group(2),
+            'regex_season_no' : int(match.group(3)),
+            'regex_episode_no' : int(match.group(4)),
+            'regex_extension' : match.string.split('.')[-1]
+         } if match else None
 
         return output
 
@@ -57,10 +55,8 @@ class Renamer:
 
         result_list = []
         
-        # print('For search term: {}'.format(search_term))
         for item in search_res:
             diff = [li for li in difflib.ndiff(search_term.lower(), item['seriesName'].lower()) if li[0] != ' ']
-            # print('[{}] Returned: "{}" with [{} diff].'.format(index, item['seriesName'],len(diff)))
             result_list.append(Cobjs.SearchResult(
                 show_title = item['seriesName'],
                 id = item['id'],
@@ -78,34 +74,34 @@ class Renamer:
                     return result
         return None
 
-    def get_episode_title(self, show_id,season_no,episode_no):
+    def get_episode_data(self, show_id,season_no,episode_no):
         try:
-            ep = self.t[show_id][int(season_no)][int(episode_no)]
-            return ep['episodename']
+            ep = self.t[show_id][season_no][episode_no]
+            return ep
         except:
             raise
 
     def get_ep_tvdb_info(self, regex_data):
-        best_match = self._best_match_show(regex_data.text_show_title, regex_data.show_year)
-        episode_title = self.get_episode_title(best_match.id,regex_data.season_no,regex_data.episode_no)
+        best_match = self._best_match_show(regex_data['regex_show_title'], regex_data['regex_show_year'])
+        tvdb_episode = self.get_episode_data(best_match.id,regex_data['regex_season_no'],regex_data['regex_episode_no'])
 
-        return Cobjs.Episode(
-            original_file_name = regex_data.original_file_name,
-            id = best_match.id,
-            text_show_title = regex_data.text_show_title,
-            tvdb_show_title = best_match.show_title,
-            show_year = regex_data.show_year,
-            season_no = regex_data.season_no,
-            episode_no = regex_data.episode_no,
-            episode_title = episode_title,
-            extension = regex_data.extension
-            )
+        return {
+            'tvdb_id' : best_match.id,
+            'tvdb_show_title' : best_match.show_title,
+            'tvdb_episode_title' : tvdb_episode['episodename'],
+        }
 
-    def format_filename(self, episode, format_str):
-        return format_str.format(episode.tvdb_show_title,int(episode.season_no),int(episode.episode_no),episode.episode_title,episode.extension)
+    def format_filename(self, show_title: str, s_no: int, ep_no: int, ep_name: str, ext: str, format_str: str):
+        return format_str.format(show_title, s_no, ep_no, ep_name, ext)
 
-    def get_new_filename(self, tvdb_ep_data):
-        new_filename = self.format_filename(tvdb_ep_data, self.OUTPUT_FORMAT_STRING)
+    def get_new_filename(self, regex_data, tvdb_ep_data):
+        new_filename = self.format_filename(tvdb_ep_data['tvdb_show_title'],
+                                            regex_data['regex_season_no'],
+                                            regex_data['regex_episode_no'],
+                                            tvdb_ep_data['tvdb_episode_title'],
+                                            regex_data['regex_extension'],
+                                            self.OUTPUT_FORMAT_STRING
+                                            )
         if self.MAKEWINSAFE:
             return self.make_winsafe(new_filename)
         else:
@@ -127,14 +123,14 @@ class Renamer:
         if regex_data:
             try:
                 tvdb_ep_data = self.get_ep_tvdb_info(regex_data)
-                show_dir = self.make_winsafe(tvdb_ep_data.tvdb_show_title)
-                season_dir = self.season_dir_format.format(int(tvdb_ep_data.season_no))
-                new_filename = self.get_new_filename(tvdb_ep_data)
+                show_dir = self.make_winsafe(tvdb_ep_data['tvdb_show_title'])
+                season_dir = self.season_dir_format.format(regex_data['regex_season_no'])
+                new_filename = self.get_new_filename(regex_data, tvdb_ep_data)
                 return os.path.join(show_dir,season_dir,new_filename)
             except:
-                print('Err      Show-name TVDB search returned zero results for "{}" from : {}'.format(regex_data.text_show_title,regex_data.original_file_name))
-                return None
+                print('Err      Show-name TVDB search returned zero results for "{}" from : {}'.format(regex_data['regex_show_title'],regex_data['original_file_name']))
+                raise
         else:
             print('Err      Filename cannot be parsed for "{}"!'.format(orig_filename))
-            #add ability to manually select show and episodes?
+            #add ability to manually select show and season / episode number?
             return None
